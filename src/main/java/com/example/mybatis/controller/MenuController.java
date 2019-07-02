bdb873c37eff0082d467708c7ba35fdbc774ffbc
LiLixikun <460373604@qq.com>
2019-07-02 09:31:25 +0800

    mybatis自动生成

--- src/main/java/com/example/mybatis/controller/MenuController.java
+++ src/main/java/com/example/mybatis/controller/MenuController.java
@@ -1,5 +1,8 @@
 package com.example.mybatis.controller;
 
+import com.example.mybatis.entity.Menu;
+import com.example.mybatis.mapper.MenuMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/menu")
 public class MenuController {
 
+    @Autowired
+    private MenuMapper mapper;
+
     @GetMapping("/list")
-    public String getMenus(){
-        return "Hello word";
+    public Menu getMenus(){
+
+        return mapper.selectByPrimaryKey(4);
     }
 }
---
+++ src/main/java/com/example/mybatis/entity/Menu.java
@@ -0,0 +1,53 @@
+package com.example.mybatis.entity;
+
+public class Menu {
+    private Integer id;
+
+    private String menuName;
+
+    private String menuIcon;
+
+    private String link;
+
+    private Integer pid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName == null ? null : menuName.trim();
+    }
+
+    public String getMenuIcon() {
+        return menuIcon;
+    }
+
+    public void setMenuIcon(String menuIcon) {
+        this.menuIcon = menuIcon == null ? null : menuIcon.trim();
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link == null ? null : link.trim();
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+}
